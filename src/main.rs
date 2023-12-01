c30e790661361122d7be191f610626190b4b5ccd
Richard Lalonde <lalonder@wwt.com>
2023-11-30 22:43:35 -0600

    Attempt at stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 // Uncomment this block to pass the first stage
 use std::net::UdpSocket;
 
-struct DnsMessage {
-    id: &[str] = b"1234",
-
-}
-
-fn main() -> std::io::Result<()> {
+fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    let udp_socket = UdpSocket::bind("127.0.0.1:2053")?;
+    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to do stuff.");
     let mut buf = [0; 12];
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 let headers = String::from_utf8_lossy(&buf[0..size]);
-                println!("Received {} bytes from {}: {}", size, source, &headers);
-                let response = headers;
+                println!("Received {} bytes from {}: {}", size, source, headers);
+                let response = [];
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
