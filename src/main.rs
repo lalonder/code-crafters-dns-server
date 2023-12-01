3bbc8e91070017b0758ab166d39723f31db2ba0a
Richard Lalonde <lalonder@wwt.com>
2023-11-30 23:56:49 -0600

    Add DnsMessage struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 // Uncomment this block to pass the first stage
 use std::net::UdpSocket;
 
+#[derive(Debug)]
+struct DnsMessage {
+    header: [u8; 12]
+}
+
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -11,12 +17,12 @@ fn main() {
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                let headers = &buf[..12];
+                let headers = <[u8; 12]>::try_from(&buf[..12]).unwrap();
                 let body = std::str::from_utf8(&buf[12..size]).expect("Failed to parse body");
                 println!("Received {} bytes from {}: {:?} {}", size, source, headers, body);
-                let response = [];
+                let response = DnsMessage { header: headers };
                 udp_socket
-                    .send_to(&response, source)
+                    .send_to(&response.header, source)
                     .expect("Failed to send response");
             }
             Err(e) => {
