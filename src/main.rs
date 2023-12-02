ced0557219d3d128a1ca74207677f41942941439
Richard Lalonde <lalonder@wwt.com>
2023-12-02 15:47:56 -0600

    CLI tests (cli-test-1701553676924)

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,66 @@
-// Uncomment this block to pass the first stage
 use std::net::UdpSocket;
 
 #[derive(Debug)]
-struct DnsMessage {
-    header: [u8; 12]
+struct DnsHeader {
+    id: u16,
+    flags: [u8; 2],
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
 }
 
-impl DnsMessage {
-    fn respond(&mut self) {
-        self.header[2] += 0b1000_0000u8;
+#[derive(Debug)]
+struct DnsMessage {
+    header: DnsHeader,
+}
+
+impl From<&[u8]> for DnsHeader {
+    fn from(buffer: &[u8]) -> Self {
+        DnsHeader {
+            id:         u16::from_be_bytes([buffer[0], buffer[1]]),
+            flags:      [buffer[2], buffer[3]],
+            qdcount:    u16::from_be_bytes([buffer[4], buffer[5]]),
+            ancount:    u16::from_be_bytes([buffer[6], buffer[7]]),
+            nscount:    u16::from_be_bytes([buffer[8], buffer[9]]),
+            arcount:    u16::from_be_bytes([buffer[10], buffer[11]]),
+        }
     }
 }
 
+fn get_header_array(header: &DnsHeader) -> [u16; 6] {
+    [header.id, u16::from_be_bytes(header.flags), header.qdcount, header.ancount, header.nscount, header.arcount]
+}
+
+impl From<&[u8]> for DnsMessage {
+    fn from(buffer: &[u8]) -> Self {
+        DnsMessage {
+            header: DnsHeader::from(&buffer[..12]),
+        }
+    }
+}
+
+impl DnsHeader {
+    fn set_response_flag(&mut self) {
+        self.flags[0] += 0b1000_0000
+    }
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to socket.");
     let mut buf = [0; 512];
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                let mut message = DnsMessage { header: <[u8; 12]>::try_from(&buf[..12]).unwrap() };
-                println!("Received {} bytes from {}: {:?}", size, source, message.header);
-                message.respond();
-                udp_socket
-                    .send_to(&message.header, source)
-                    .expect("Failed to send response");
+                let mut message = DnsMessage::from(&buf[..]);
+                println!("Received {} bytes from {}: {:?}", size, source, get_header_array(&message.header));
+                message.header.set_response_flag();
+                println!("{:?}", message);
+                // udp_socket
+                   // .send_to(, source)
+                   // .expect("Failed to send response");
             }
             Err(e) => {
                 eprintln!("Error receiving data: {}", e);
