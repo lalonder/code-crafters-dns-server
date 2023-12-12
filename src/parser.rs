4cbc6d34b35a849bd12eac5783b6112136af7707
Richard Lalonde <lalonder@wwt.com>
2023-12-12 06:21:18 -0600

    CLI tests (cli-test-1702383678627)

--- src/parser.rs
+++ src/parser.rs
@@ -1,54 +1,51 @@
 use nom::bytes::complete::{take, take_until};
 use nom::IResult;
 use nom::sequence::tuple;
+use nom::number::complete::be_u16;
 use crate::{Header, Question};
+use crate::parser::bits::parse_flags;
 
-pub fn parse_flags(bytes: &[u8; 2]) -> [u8; 2] {
-    let qr =     bytes[0] & 0b1000_0000;
-    let opcode = bytes[0] & 0b0111_1000;
-    let aa =     0b0000_0000u8;
-    let tc =     0b0000_0000u8;
-    let rd =     bytes[0] & 0b0000_0001;
-
-    let ra =     bytes[1] & 0b1000_0000;
-    let z =      0b0000_0000;
-    let rcode =  if opcode == 0u8 { 0u8 } else { 4u8 };
-    [qr+opcode+aa+tc+rd, ra+z+rcode]
+pub fn parse_message(input: &[u8]) -> IResult<&[u8], (Header, Question, Question)> {
+    tuple((parse_header, parse_question, parse_question))(input)
 }
 
 pub fn parse_header(input: &[u8]) -> IResult<&[u8], Header> {
-    let (
-        input,
-        (id, flags, qdcount, ancount, nscount, arcount,),
-    ) = tuple((take_two_bytes, take_two_bytes, take_two_bytes, take_two_bytes, take_two_bytes, take_two_bytes))(input)?;
-    Ok((
-        input,
-        Header { id, flags, qdcount, ancount, nscount, arcount, }
-    ))
+    match tuple((
+        be_u16,
+        parse_flags,
+        be_u16,
+        be_u16,
+        be_u16,
+        be_u16,
+    ))(input) {
+        Ok((input, (id, flags, qdcount, ancount, nscount, arcount))) =>
+            Ok((
+                input,
+                Header { id, flags, qdcount, ancount, nscount, arcount, },
+            )),
+        _ => panic!(),
+    }
 }
 
 pub fn parse_question(input: &[u8]) -> IResult<&[u8], Question> {
-    let (
-        input,
-        (qname, qtype, qclass)
-    ) = tuple((until_null, take_two_bytes, take_two_bytes))(input)?;
-    Ok((
-        input,
-        Question { qname, qtype, qclass, }
-    ))
+    match tuple((
+        until_null,
+        take(1usize),
+        be_u16,
+        be_u16,
+    ))(input) {
+        Ok((input, (qname, _null, qtype, qclass))) => {
+            let mut qname = Vec::from(qname);
+            qname.push(0x0);
+            Ok(( input, Question { qname, qtype, qclass } ))
+        },
+        _ => panic!(),
+    }
 }
 
-fn take_two_bytes(input: &[u8]) -> IResult<&[u8], [u8; 2]> {
-    let (input, bytes) = take(2usize)(input)?;
-    Ok((input, [bytes[0], bytes[1]]))
+fn until_null(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    take_until("\x00")(input)
 }
-
-fn until_null(input: &[u8]) -> IResult<&[u8], Vec<u8>> {
-    let (input, output) = take_until("\x00")(input)?;
-    Ok((input, Vec::from(output)))
-
-}
-
 
 pub mod bits {
     use nom::IResult;
@@ -56,9 +53,13 @@ pub mod bits {
     use nom::sequence::tuple;
     use nom::bits::complete::take;
     use nom::bits::bits;
+    use crate::Flags;
 
-    pub fn parse_header_flags(input: &[u8]) -> IResult<&[u8], (u8,u8,u8,u8,u8,u8,u8,u8)> {
-        bits::<_, _, Error<(&[u8], usize)>, _, _,>(
+    type IResultFlags<'a> = (&'a [u8], (u8, u8, u8, u8, u8, u8, u8, u8));
+
+    pub fn parse_flags(input: &[u8]) -> IResult<&[u8], Flags> {
+        let (input, (qr, opcode, aa, tc, rd, ra, z, _rcode)): IResultFlags =
+            bits::<_, _, Error<(&[u8], usize)>, _, _,>(
             tuple((
                 take(1usize),
                 take(4usize),
@@ -68,6 +69,22 @@ pub mod bits {
                 take(1usize),
                 take(3usize),
                 take(4usize),
-            )))(input)
+            )))(input)?;
+        Ok((
+            input,
+            Flags {
+                qr: qr << 7,
+                opcode: opcode << 3,
+                aa: aa << 2,
+                tc: tc << 1,
+                rd,
+                ra: ra << 7,
+                z: z << 4,
+                rcode: match opcode {
+                    0x0 => 0x0,
+                    _ => 0x4
+                },
+            }
+        ))
     }
 }
